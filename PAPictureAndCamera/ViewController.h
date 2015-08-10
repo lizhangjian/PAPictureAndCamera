9ff2e6451ea008975b8f76c491cc2cbd7fba17a1
myMac <mymac@myMacdeMacBook-Air.local>
2015-08-10 08:51:55 +0800

    Initial Commit

---
+++ PAPictureAndCamera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PAPictureAndCamera
+//
+//  Created by myMac on 15/8/10.
+//  Copyright (c) 2015年 myMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
